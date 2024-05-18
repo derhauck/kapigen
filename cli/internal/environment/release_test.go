b1c11e029291addc4717212d0a23e22f1dac7aa4
DERHauck <d.hauck@netzwolke.com>
2024-05-18 15:13:12 +0200

    fix release checks #2

--- cli/internal/environment/release_test.go
+++ cli/internal/environment/release_test.go
@@ -23,12 +23,13 @@ func TestIsRelease(t *testing.T) {
 			t.Errorf("should be true as env was prepared id: %s, commit: %s, default :%s, tag:%s", CI_MERGE_REQUEST_ID.Get(), CI_COMMIT_BRANCH.Get(), CI_DEFAULT_BRANCH.Get(), CI_COMMIT_TAG.Get())
 		}
 	})
-	t.Run("will not work when not on default branch", func(t *testing.T) {
+	t.Run("will not work without tag", func(t *testing.T) {
 		CI_MERGE_REQUEST_ID.Set("123")
 		CI_COMMIT_BRANCH.Set("feature")
 		CI_DEFAULT_BRANCH.Set("master")
+		CI_COMMIT_TAG.Unset()
 		if IsRelease() {
-			t.Errorf("should be false as commit is not default branch, %s", CI_COMMIT_BRANCH.Get())
+			t.Errorf("should be false as tag is not set, %s", CI_COMMIT_TAG.Get())
 		}
 	})
 }
