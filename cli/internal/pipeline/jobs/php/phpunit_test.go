55405bc1126e61a25fb41286c26611e908086d86
DERHauck <d.hauck@netzwolke.com>
2024-05-29 23:28:52 +0200

    add unit tests

--- cli/internal/pipeline/jobs/php/phpunit_test.go
+++ cli/internal/pipeline/jobs/php/phpunit_test.go
@@ -6,7 +6,7 @@ func TestNewPhpUnit(t *testing.T) {
 	t.Run("Can create phpunit job", func(t *testing.T) {
 		t.Parallel()
 		t.Run("Correct parameters", func(t *testing.T) {
-			job, err := NewPhpUnit("testimage", "testpath", "test", ".", "", map[string]int32{})
+			job, err := NewPhpUnit("testimage", "testpath", "test", ".", "", "testpath", map[string]int32{})
 			if err != nil {
 				t.Error(err)
 			}
@@ -19,13 +19,13 @@ func TestNewPhpUnit(t *testing.T) {
 			}
 		})
 		t.Run("Incorrect parameters", func(t *testing.T) {
-			job, err := NewPhpUnit("", "", "", "", "", map[string]int32{})
-			if err != nil {
-				t.Error(err)
-			}
-			err = job.Render()
+			_, err := NewPhpUnit("", "", "", "", "", "testpath", map[string]int32{})
 			if err == nil {
-				t.Errorf("created job succeeded without image and path but should not: %s", job.CiJobYaml.String())
+				t.Error("Should be missing args error for 'imageName'")
+			}
+			_, err = NewPhpUnit("testimage", "", "", "", "", "testpath", map[string]int32{})
+			if err == nil {
+				t.Error("Should be missing args error for 'composerPath'")
 			}
 
 		})
