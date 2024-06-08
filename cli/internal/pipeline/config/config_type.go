9d1448450c2860234638a0fa4de71da02765e2e1
DERHauck <d.hauck@netzwolke.com>
2024-06-08 23:51:03 +0200

    remove infra pipeline since it is not ready yet

--- cli/internal/pipeline/config/config_type.go
+++ cli/internal/pipeline/config/config_type.go
@@ -3,15 +3,13 @@ package config
 import "kapigen.kateops.com/internal/pipeline/types"
 
 const (
-	InfrastructurePipeline types.PipelineType = "infrastructure"
-	DockerPipeline         types.PipelineType = "docker"
-	PHPPipeline            types.PipelineType = "php"
-	GOLANG                 types.PipelineType = "golang"
+	DockerPipeline types.PipelineType = "docker"
+	PHPPipeline    types.PipelineType = "php"
+	GOLANG         types.PipelineType = "golang"
 )
 
 var PipelineConfigTypes = map[types.PipelineType]types.PipelineConfigInterface{
-	DockerPipeline:         &Docker{},
-	InfrastructurePipeline: &Infrastructure{},
-	GOLANG:                 &Golang{},
-	PHPPipeline:            &Php{},
+	DockerPipeline: &Docker{},
+	GOLANG:         &Golang{},
+	PHPPipeline:    &Php{},
 }
