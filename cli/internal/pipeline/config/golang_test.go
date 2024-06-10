39e6e810c6c2979b79e39591c674279776670f2e
DERHauck <d.hauck@netzwolke.com>
2024-06-10 13:19:37 +0200

    add golang tests

--- cli/internal/pipeline/config/golang_test.go
+++ cli/internal/pipeline/config/golang_test.go
@@ -29,3 +29,30 @@ func TestGolangAutoConfig(t *testing.T) {
 		})
 	}
 }
+
+func TestGolang_Validate(t *testing.T) {
+	t.Run("can validate golang", func(t *testing.T) {
+		golang := &Golang{
+			ImageName: "golang:1.21",
+			Path:      "cli",
+		}
+		if err := golang.Validate(); err != nil {
+			t.Errorf("should not have error: %s", err)
+		}
+		if golang.ImageName != "golang:1.21" {
+			t.Errorf("should be equal")
+		}
+		if golang.Path != "cli" {
+			t.Errorf("should be equal")
+		}
+	})
+	t.Run("can not validate golang", func(t *testing.T) {
+		golang := &Golang{
+			ImageName: "",
+		}
+		if err := golang.Validate(); err == nil {
+			t.Error("should have error")
+		}
+
+	})
+}
