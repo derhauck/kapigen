d994d3feda46672b1a07924ba84abbf551dd2d4d
DERHauck <d.hauck@netzwolke.com>
2024-06-25 02:44:01 +0200

    add generic test

--- cli/internal/pipeline/config/generic_test.go
+++ cli/internal/pipeline/config/generic_test.go
@@ -1,10 +1,52 @@
 package config
 
 import (
+	"reflect"
 	"testing"
+
+	"kapigen.kateops.com/factory"
+	"kapigen.kateops.com/internal/cli"
+	"kapigen.kateops.com/internal/docker"
+	"kapigen.kateops.com/internal/gitlab/stages"
+	"kapigen.kateops.com/internal/version"
 )
 
 func TestGeneric_Build(t *testing.T) {
+	t.Run("can build a generic pipeline", func(t *testing.T) {
+		expectedScripts := []string{"ls", "pwd"}
+		generic := &Generic{
+			Stage:   stages.TEST.String(),
+			Scripts: expectedScripts,
+		}
+		main := factory.New(&cli.Settings{
+			Mode:         version.Gitlab,
+			PrivateToken: "",
+		})
+		err := generic.Validate()
+		jobs, err := generic.Build(main, "generic", "generic")
+		if err != nil {
+			t.Error(err)
+		}
+		if jobs == nil {
+			t.Error("jobs is nil")
+			t.Fail()
+		}
+		genericJob := jobs.GetJobs()[0]
+		if genericJob == nil {
+			t.Error("genericJob is nil")
+			t.Fail()
+		}
+		err = genericJob.Render()
+		if err != nil {
+			t.Error(err)
+		}
+		if genericJob.CiJob.Image.Name != docker.Alpine_3_18.String() {
+			t.Errorf("expected image name to be %s, received %s", docker.Alpine_3_18.String(), genericJob.CiJob.Image.Name)
+		}
+		if !reflect.DeepEqual(genericJob.CiJobYaml.Script, expectedScripts) {
+			t.Errorf("expected scripts to be %s, received %s", expectedScripts, genericJob.CiJobYaml.Script)
+		}
+	})
 
 }
 
