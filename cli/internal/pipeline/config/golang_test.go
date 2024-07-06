a54b62aff29dfc42d0c2c488b0997d1923cada8a
DERHauck <d.hauck@netzwolke.com>
2024-07-06 18:16:12 +0200

    fix broken unit test #15

--- cli/internal/pipeline/config/golang_test.go
+++ cli/internal/pipeline/config/golang_test.go
@@ -23,7 +23,6 @@ func TestGolangAutoConfig(t *testing.T) {
 		},
 	}
 	environment.SetLocalEnv()
-	environment.CI_PROJECT_DIR.Set("/app")
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := GolangAutoConfig(); !reflect.DeepEqual(got, tt.want) {
